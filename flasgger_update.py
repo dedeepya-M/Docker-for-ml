04aebc3a636d107e880e8ade78160cbf61969b68
Dedeepya-M <dedeepyamogilisetti@gmail.com>
2023-05-15 11:15:44 +0530

    flasgger done

--- flasgger_update.py
+++ flasgger_update.py
@@ -16,6 +16,32 @@ def welcome():
 
 @app.route('/predict',methods=["Get"])
 def predict_note_authentication():
+    
+    """Let's Authenticate the Banks Note 
+    This is using docstrings for specifications.
+    ---
+    parameters:  
+      - name: variance
+        in: query
+        type: number
+        required: true
+      - name: skewness
+        in: query
+        type: number
+        required: true
+      - name: curtosis
+        in: query
+        type: number
+        required: true
+      - name: entropy
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values
+        
+    """
     variance=request.args.get("variance")
     skewness=request.args.get("skewness")
     curtosis=request.args.get("curtosis")
@@ -26,6 +52,20 @@ def predict_note_authentication():
 
 @app.route('/predict_file',methods=["POST"])
 def predict_note_file():
+    """Let's Authenticate the Banks Note 
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: file
+        in: formData
+        type: file
+        required: true
+      
+    responses:
+        200:
+            description: The output values
+        
+    """
     df_test=pd.read_csv(request.files.get("file"))
     print(df_test.head())
     prediction=classifier.predict(df_test)
